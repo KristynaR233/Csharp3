955cc9b9249071e8dcf2b54a1eb1ec1477e81bdd
Kristyna Randova <randova.kristyna@seznam.cz>
2025-11-17 20:24:38 +0100

    mock PostTests

--- ToDoList/tests/ToDoList.Test/UnitTests/PostTests.cs
+++ ToDoList/tests/ToDoList.Test/UnitTests/PostTests.cs
@@ -1,42 +1,44 @@
-using System;
 using NSubstitute;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.DTOs;
+using ToDoList.WebApi;
+using ToDoList.Persistence.Repositories;
+using ToDoList.Domain.Models;
+using ToDoList.Test;
 
-namespace ToDoList.tests.UnitTests
+namespace ToDoList.Test.UnitTests
 {
-public class PostTests
-{
-
-    [Fact]
-    public void Post_ValidRequest_ReturnNewItem()
+    public class PostTests
     {
-        // Arrange
-        var repositoryMock = Substitute.For<IRepository<ToDoItem>();
-        var controller = new ToDoItemsController(null, repositoryMock);
-        var request = new ToDoItemCreateRequestDto(
-            Name: "Jmeno",
-            Description: "Popis",
-            IsCompleted: false
-        );
 
-        // Act
-        var result = controller.Create(request);
-        var value = result.GetValue();
+        [Fact]
+        public void Post_ValidRequest_ReturnNewItem()
+        {
+            // Arrange
+            var repositoryMock = Substitute.For<IRepository<ToDoItem>>();
+            var controller = new ToDoItemsController(null, repositoryMock);
+            var request = new ToDoItemCreateRequestDto(
+                Name: "Jmeno",
+                Description: "Popis",
+                IsCompleted: false
+            );
+
+            // Act
+            var result = controller.Create(request);
+            var value = result.GetValue();
 
 
-        // Assert
-        Assert.IsType<CreatedAtActionResult>(result.Result);
-        Assert.NotNull(value);
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(result.Result);
+            Assert.NotNull(value);
 
-        Assert.Equal(request.Description, value.Description);
-        Assert.Equal(request.Name, value.Name);
-        Assert.Equal(request.IsCompleted, value.IsCompleted);
+            Assert.Equal(request.Description, value.Description);
+            Assert.Equal(request.Name, value.Name);
+            Assert.Equal(request.IsCompleted, value.IsCompleted);
+        }
+
+
+
+
     }
-
-
-
-
 }
-    }
