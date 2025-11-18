c70af28a085ef16c8c55ab37fbe01243e8680e0f
Kristyna Randova <randova.kristyna@seznam.cz>
2025-11-18 19:01:12 +0100

    frontend razor

---
+++ ToDoList/src/ToDoList.Frontend/Components/Pages/BreakoutRoom1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoList.Frontend.Components.Pages
+{
+    public class BreakoutRoom1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
