6eb1a3893421560d3942da463f5f597b8d41cd3d
Kristyna Randova <randova.kristyna@seznam.cz>
2025-12-09 18:29:55 +0100

    uprava konfigurace

--- ToDoList/src/ToDoList.Frontend/Program.cs
+++ ToDoList/src/ToDoList.Frontend/Program.cs
@@ -6,8 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 builder.Services.AddScoped<IToDoItemsClient,ToDoItemsClient>();
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.Configuration["ToDoItemApiAddress"])});
 
 
 var app = builder.Build();
